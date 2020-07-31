2c1be56cb56509218974fbc26b5e90b1191adb71
Natalia Davydova <davydovanatalia063@gmail.com>
2020-07-31 15:15:51 -0700

    added hero section, header section, advantages section, delievery-goods section, added fonts and interface of button

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('.delievery-slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-right.svg" alt=""></button>',
+    // autoplay: true,
+    fade: true
+  });
+
+});
